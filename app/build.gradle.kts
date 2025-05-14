c09e8854f355b985aed998f9b04a26ff883e88d8
Juan Pacheco Kleim <pachkleim1918@gmail.com>
2025-05-14 15:07:12 -0300

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.example.sayo_chat"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.sayo_chat"
+        minSdk = 29
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+    //Dependências Firebase
+    implementation(platform("com.google.firebase:firebase-bom:33.13.0"))
+    //Firebase Analytics
+    implementation("com.google.firebase:firebase-analytics")
+    //Firebase Auth
+    implementation("com.google.firebase:firebase-auth")
+    //Firebase Firestore
+    implementation("com.google.firebase:firebase-firestore")
+    //Firebase Storage
+    implementation("com.google.firebase:firebase-storage")
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
