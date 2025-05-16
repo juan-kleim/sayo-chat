fedbb45bdf1b6be7011c1c0ccb42620fa6aa87fb
Juan Pacheco Kleim <pachkleim1918@gmail.com>
2025-05-16 11:08:03 -0300

    Ação de ir para a tela de cadastro e voltar para a tela de login

--- app/src/main/java/com/example/sayo_chat/iniciosayochat/CadastroActivity.kt
+++ app/src/main/java/com/example/sayo_chat/iniciosayochat/CadastroActivity.kt
@@ -6,16 +6,35 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.sayo_chat.R
+import com.example.sayo_chat.databinding.ActivityCadastroBinding
 
 class CadastroActivity : AppCompatActivity() {
+
+    private val binding by lazy{
+        ActivityCadastroBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_cadastro)
+        setContentView( binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        inicializarToolbar()
+    }
+
+    private fun inicializarToolbar() {
+        val toolbar = binding.includeToolbar.tbPrincipal
+        setSupportActionBar( toolbar )
+        supportActionBar?.apply{
+            title = "Cadastre-se"
+            setDisplayHomeAsUpEnabled(true)
+        }
+
     }
 }
